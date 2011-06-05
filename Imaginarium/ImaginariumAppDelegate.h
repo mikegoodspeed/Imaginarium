f96c82cd5671cb89428fb06ab5e34767e01b7efe
Mike Goodspeed <mgoodspeed@getcollc.com>
2011-06-05 18:31:45 +0100

    Initial Commit

---
+++ Imaginarium/ImaginariumAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ImaginariumAppDelegate.h
+//  Imaginarium
+//
+//  Created by Mike Goodspeed on 6/5/11.
+//  Copyright 2011 Mike Goodspeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImaginariumAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
